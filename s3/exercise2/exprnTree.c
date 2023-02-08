0e3367dccc884adf49a7774887077a068cdf6d48
abhi_ram <abhiramjayapuri2015@gmail.com>
2023-02-08 18:54:41 +0530

    completed exercise2

--- s3/exercise2/exprnTree.c
+++ s3/exercise2/exprnTree.c
@@ -284,7 +284,7 @@ reg_index codeGen(struct node *root, FILE *fp){
         fprintf(fp, "JZ R%d, L%d\n", r1, label1);
         freeReg();
         if ( root->right->nodetype == elseNode){
-            // fprintf (fp, "ethi");
+            
             r1 = codeGen(root->right->left, fp);
             freeReg();
             fprintf (fp, "JMP L%d\n", label2);
@@ -293,7 +293,7 @@ reg_index codeGen(struct node *root, FILE *fp){
             freeReg();
             fprintf (fp, "L%d:\n", label2);
         }else{
-            // fprintf (fp, "oombi");
+           
             r1 = codeGen(root->right, fp);
             freeReg();
             fprintf (fp, "L%d:\n", label1);
